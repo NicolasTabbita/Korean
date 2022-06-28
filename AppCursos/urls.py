fa0e8481505c0c701fd6c49ae9ee5b77312b57cf
NTabbita <nico.tabbita@gmail.com>
2022-06-28 19:11:58 -0300

    se modificaron las urls con una mejor estructura

--- AppCursos/urls.py
+++ AppCursos/urls.py
@@ -5,10 +5,10 @@ app_name = 'cursos'
 
 urlpatterns = [
     path('disponibles/', views.capacitaciones, name="capacitaciones"),
-    path('capacitacionDetalle/<id>', views.capacitacionDetalle, name="capacitacion_detalle"),
-    path('nuevaCapacitacion/', views.crearCapacitacion, name="nueva_capacitacion"),
-    path('eliminarCapacitacion/<id>', views.eliminarCapacitacion, name="eliminar_capacitacion"),
-    path('editarCapacitacion/<id>', views.editarCapacitacion, name="editar_capacitacion"),
+    path('disponibles/detalle/<id>', views.capacitacionDetalle, name="capacitacion_detalle"),
+    path('disponibles/crear', views.crearCapacitacion, name="nueva_capacitacion"),
+    path('disponibles/eliminar/<id>', views.eliminarCapacitacion, name="eliminar_capacitacion"),
+    path('disponibles/editar/<id>', views.editarCapacitacion, name="editar_capacitacion"),
     path('foro/comentario/nuevo/<id>', views.crearComentario, name="nuevo_comentario"),
     path('foro/comentario/eliminar/<id>', views.eliminarComentario, name="eliminar_comentario"),
     path('foro/comentario/respuesta/<id>', views.crearRespuesta, name="nueva_respuesta"),
--- AppTienda/urls.py
+++ AppTienda/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from AppTienda import views
 from django.contrib.auth.views import LogoutView
 urlpatterns = [
-    path('agregarProducto/<id>', views.agregarProducto, name="agregar_producto"),
-    path('eliminarProducto/<id>', views.eliminarProducto, name="eliminar_producto"),
+    path('producto/nuevo/<id>', views.agregarProducto, name="agregar_producto"),
+    path('producto/eliminar/<id>', views.eliminarProducto, name="eliminar_producto"),
     path('carrito/', views.verCarrito, name="carrito"),
     path('comprar/', views.comprar, name="comprar"),
-    path('misCapacitaciones/', views.misCursos, name="mis_capacitaciones"),
-    path('registrarse/', views.crearUsuario, name="registro_usuario"),
-    path('login/', views.loginRequest, name="login"),
-    path('perfil/', views.editarUsuario, name="editar_usuario"),
-    path('logout/', LogoutView.as_view(template_name = 'AppTienda/logout.html'), name = 'logout'),
-    path('acercaDeNosotros/', views.acerca, name='acerca_de_nosotros'),
+    path('usuario/capacitaciones/', views.misCursos, name="mis_capacitaciones"),
+    path('usuario/registrarse/', views.crearUsuario, name="registro_usuario"),
+    path('usuario/login/', views.loginRequest, name="login"),
+    path('usuario/perfil/', views.editarUsuario, name="editar_usuario"),
+    path('usuario/logout/', LogoutView.as_view(template_name = 'AppTienda/logout.html'), name = 'logout'),
+    path('about/', views.acerca, name='acerca_de_nosotros'),
 ]
